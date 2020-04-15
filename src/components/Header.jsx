025ef770a39414153398864bb490b3ba14d1db46
karolina <karolina.frostare@gmail.com>
2020-04-15 06:28:24 +0200

    inserts css link to index html, updates index css, updates header and app jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-import React from 'react';
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Brillo</h1>
+    </div>
+  );
+}
+
+export default Header;
