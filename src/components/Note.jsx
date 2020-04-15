dea42bf252f3d469d4591f3184ac1577820a3150
karolina <karolina.frostare@gmail.com>
2020-04-15 07:24:24 +0200

    creates note component, connect notes to app jsx, updates styling

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Awesome Title</h1>
+      <p>Awesome Idea</p>
+    </div>
+  )
+}
+
+export default Note;
