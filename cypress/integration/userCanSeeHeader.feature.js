2ee1e38ea8bc56bfe9fae3eb151ea05ca4193b98
karolina <karolina.frostare@gmail.com>
2020-04-15 06:42:56 +0200

    updates header tests, all green

--- cypress/integration/userCanSeeHeader.feature.js
+++ cypress/integration/userCanSeeHeader.feature.js
@@ -1,7 +1,10 @@
-describe('User can se header', () => {
-  it('enters page and views header', () => {
+describe("User can se header and", () => {
+  it("shows the title", () => {
     cy.exec("yarn start");
-    cy.visit("/")
-    cy.get('#header').should('contain', 'Brillo')
-  })
-})
+    cy.visit("/");
+    cy.get("#header").should("contain", "Brillo");
+  });
+  it("does not show the footer", () => {
+    cy.get("#header").should("not.contain", "copyright");
+  });
+});
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="header">
-      <h1>Brillo</h1>
+      <h1 id="header">Brillo</h1>
     </div>
   );
 }
